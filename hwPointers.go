62275cf5b9076e7144bf007c0dea82108193b411
Gindulin Ilia <gindulin.gho@gmail.com>
2024-07-31 09:48:33 +0300

    first commit

---
+++ hwPointers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const pi = math.Pi
+
+func main() {
+	//printCircleArea(10)
+	//printRectangleArea(10, 10)
+	printRectanglePerimeter(10, 10)
+}
+
+func printCircleArea(radius int) {
+	//S = πr^2
+	area := float32(radius) * float32(radius) * pi
+	fmt.Println("Area circle:", area)
+}
+
+func printRectangleArea(lenght int, width int) {
+	//S = lenght × width
+	area := lenght * width
+	fmt.Println("Area rectangle:", area)
+}
+
+func printRectanglePerimeter(lenght int, width int) {
+	//P = 2(lenght + width)
+	perimeter := 2 * (lenght + width)
+	fmt.Println("Perimeter rectangle:", perimeter)
+}
